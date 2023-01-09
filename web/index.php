cb70c52160ed228213c480713a6abbe2f7851f51
ButterBoi <21044912@myrp,edu.sg>
2023-01-09 15:43:49 +0800

    a

--- web/index.php
+++ web/index.php
@@ -5,12 +5,13 @@
     <meta 
         name="description"
         content="testing to see if can make all attribute in Lighthouse 100%">
-</head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">`
+    </head>
 <body>
   Hello World! I wanna scream
 </body>
 </html>
 <!-- git add -A
-git commit -m "commit message goes here"
+git commit -m "a"
 git push origin main
 git push heroku main -->
